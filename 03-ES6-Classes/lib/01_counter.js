31faed5394117ec006e81456e1b259ccb4918dcc
Gheorghe Tarcea <tarcea@gmail.com>
2020-04-14 12:48:11 +0200

    words counter done

--- 03-ES6-Classes/lib/01_counter.js
+++ 03-ES6-Classes/lib/01_counter.js
@@ -1,32 +1,32 @@
 class Counter {
   constructor(text) {
     // TODO: build an internal Map of word => occurrences.
-    let noCommaText = "";
-    // split text in an array with words
-    noCommaText = text.replace(/,/g, ""); // remove commas
-    // console.log(noCommaText);
-    const myArray = noCommaText.split(" "); // each word in my text is an element in myArray
-    console.log(myArray);
-    // count the ocurence of each word in yhe array
-    let index = 0;
-    let count = 0;
-    let i;
-    while (index !== myArray.length) {
-      for (i = 0; i <= myArray.length; i += 1) {
-        if (myArray[i] === myArray[index]) {
-          count += 1;
-        }
-        console.log(`${myArray[i]}: ${count} `);
-      }
-      index += 1;
+    this.map = new Map();
+    // console.log(text.length);
+    if (text.length > 0) {
+      let noCommaText = "";
+      // split text in an array with words
+      noCommaText = text.replace(/,/g, ""); // remove commas
+      // console.log(noCommaText);
+      const myArray = noCommaText.split(" "); // each word in my text is an element in myArray
+      // console.log(myArray);
+      // count the ocurence of each word in the array
+      myArray.forEach((word) => {
+        const lowerWord = word.toLowerCase();
+        this.map.set(lowerWord, (this.map.get(lowerWord) || 0) + 1);
+      });
+
+      // build the map
+    } else {
+      return 0;
     }
-
-    // build the map
   }
-
   occurrences(word) {
     // TODO: return the number of occurrences
+    return this.map.get(word.toLowerCase()) || 0;
   }
 }
+
+module.exports = Counter;
 
 module.exports = Counter;
